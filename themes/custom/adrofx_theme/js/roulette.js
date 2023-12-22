09904363079e90b1c2a07b685441e18911765186
Ramielson <ramielsonedgar@gmail.com>
2023-12-22 22:54:58 +0200

    fix

--- themes/custom/adrofx_theme/js/roulette.js
+++ themes/custom/adrofx_theme/js/roulette.js
@@ -6,6 +6,8 @@ const rouletteBonus = document.querySelector('.roulette-base')
 
 
 window.addEventListener('DOMContentLoaded', () => {
+    $('#notification').modal('show')
+
     spinButton.forEach(button => {
         button.addEventListener('click', () => {
             rouletteBonus.style.setProperty("--target", `0`);
@@ -13,24 +15,21 @@ window.addEventListener('DOMContentLoaded', () => {
             giftImage.classList.remove('animated');
             lottie.stop();
             $(popup).modal('hide');
-          
-    
+
+
             setTimeout(() => {
                 rouletteBonus.style.setProperty("--target", `7200deg`);
                 rouletteBonus.style.animation = "spin-roulette 5s ease-in-out .2s forwards";
             }, 10);
-         
+
             setTimeout(() => {
                 $(popup).modal('show');
                 lottie.play();
                 giftImage.classList.add('animated');
             }, 5000);
-    
+
         })
     })
- 
-
- 
 
 })
 
