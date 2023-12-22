9cf31e8932b107bab8da421b31f5bf3b0de0cee7
Ramielson <ramielsonedgar@gmail.com>
2023-12-22 19:50:16 +0200

    test fix

--- themes/custom/adrofx_theme/js/roulette.js
+++ themes/custom/adrofx_theme/js/roulette.js
@@ -1,33 +1,36 @@
 const popup = document.getElementById('giftModal');
 const lottie = document.getElementById('lottie');
 const giftImage = document.querySelector('.is-animated')
-const spinButton = document.querySelector('.button-yellow')
+const spinButton = document.querySelectorAll('.js-spin')
 const rouletteBonus = document.querySelector('.roulette-base')
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    spinButton.addEventListener('click', () => {
-        rouletteBonus.style.setProperty("--target", `0`);
-        rouletteBonus.style.animation = "";
-        giftImage.classList.remove('animated');
-        lottie.stop();
-        $(popup).modal('hide');
-      
-
-        setTimeout(() => {
-            rouletteBonus.style.setProperty("--target", `7200deg`);
-            rouletteBonus.style.animation = "spin-roulette 5s ease-in-out .2s forwards";
-        }, 10);
-     
-        setTimeout(() => {
-            $(popup).modal('show');
-            lottie.play();
-            giftImage.classList.add('animated');
-        }, 5000);
-
+    spinButton.forEach(button => {
+        button.addEventListener('click', () => {
+            rouletteBonus.style.setProperty("--target", `0`);
+            rouletteBonus.style.animation = "";
+            giftImage.classList.remove('animated');
+            lottie.stop();
+            $(popup).modal('hide');
+          
+    
+            setTimeout(() => {
+                rouletteBonus.style.setProperty("--target", `7200deg`);
+                rouletteBonus.style.animation = "spin-roulette 5s ease-in-out .2s forwards";
+            }, 10);
+         
+            setTimeout(() => {
+                $(popup).modal('show');
+                lottie.play();
+                giftImage.classList.add('animated');
+            }, 5000);
+    
+        })
     })
+ 
 
-
+ 
 
 })
 
