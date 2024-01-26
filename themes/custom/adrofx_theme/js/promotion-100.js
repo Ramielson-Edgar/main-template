4257d1e8819d9d0a00d6222a0e4f604dbf4ec0c1
Ramielson <ramielsonedgar@gmail.com>
2024-01-26 18:00:36 +0200

    promo test 1

---
+++ themes/custom/adrofx_theme/js/promotion-100.js
@@ -0,0 +1,77 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+    var marqueeWhite = new Splide('#marquee-white', {
+        type: 'loop',
+        perPage: 4,
+        pagination: false,
+        arrows: false,
+        gap: '0',
+        autoScroll: {
+            speed: -1,
+            pauseOnHover: false,
+
+        },
+        breakpoints: {
+            1200: {
+                perPage: 3,
+
+            }
+        },
+
+    }).mount(window.splide.Extensions);
+
+    var marqueeDark = new Splide('#marquee-dark', {
+        type: 'loop',
+        perPage: 4,
+        pagination: false,
+        arrows: false,
+        gap: '0',
+        autoScroll: {
+            speed: 1,
+            pauseOnHover: false,
+        },
+        breakpoints: {
+            1200: {
+                perPage: 3,
+
+            }
+        },
+
+    }).mount(window.splide.Extensions);
+   
+
+    gsap.registerPlugin(ScrollTrigger);
+     
+
+    gsap.to('.icon-front', {
+        scrollTrigger:{
+            trigger: ".icon-front",
+            scrub:true,
+        },
+        yPercent:10,
+       
+    })
+
+    
+    gsap.to('.icons-back', {
+        scrollTrigger:{
+            trigger: ".icons-back",
+            scrub:true,
+        },
+        yPercent: -10,
+       
+    })
+
+ 
+    gsap.to('.image-dollar', {
+        scrollTrigger:{
+            trigger: ".image-dollar",
+            scrub:true,
+        },
+        yPercent: -10,
+        duration: .3,
+       
+    })
+  
+
+
+})
