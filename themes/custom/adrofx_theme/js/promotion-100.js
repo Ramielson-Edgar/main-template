3ea7a925f5b035c85b697ef5834557ad154526a3
Ramielson <ramielsonedgar@gmail.com>
2024-01-29 12:51:29 +0200

    marquee

--- themes/custom/adrofx_theme/js/promotion-100.js
+++ themes/custom/adrofx_theme/js/promotion-100.js
@@ -1,57 +1,39 @@
-window.addEventListener('DOMContentLoaded', ()=> {
-    var marqueeWhite = new Splide('#marquee-white', {
-        type: 'loop',
-        perPage: 4,
-        pagination: false,
-        arrows: false,
-        gap: '0',
-        autoScroll: {
-            speed: -1,
-            pauseOnHover: false,
+window.addEventListener('DOMContentLoaded', () => {
 
-        },
-        breakpoints: {
-            1200: {
-                perPage: 3,
-
-            }
-        },
-
-    }).mount(window.splide.Extensions);
-
-    var marqueeDark = new Splide('#marquee-dark', {
-        type: 'loop',
-        perPage: 4,
-        pagination: false,
-        arrows: false,
-        gap: '0',
-        autoScroll: {
-            speed: 1,
-            pauseOnHover: false,
-        },
-        breakpoints: {
-            1200: {
-                perPage: 3,
-
-            }
-        },
-
-    }).mount(window.splide.Extensions);
-   
 
     gsap.registerPlugin(ScrollTrigger);
-     
 
-    gsap.to('.icon-front', {
-        scrollTrigger:{
-            trigger: ".icon-front",
-            scrub:true,
-        },
-        yPercent:10,
-       
-    })
-
+    const marqueeWhite = gsap.utils.toArray('.marquee.white .list .list__item');
+    const marqueeDark = gsap.utils.toArray('.marquee.dark .list .list__item');
     
+    gsap.set(".marquee .list", {xPercent: -50});
+
+    gsap.to(marqueeWhite, {
+        xPercent: 100,
+        repeat: -1,
+        duration: 10,
+        ease: "linear"
+     }).totalProgress(0.5);
+
+     gsap.to(marqueeDark, {
+        xPercent: -100,
+        repeat: -1,
+        duration: 10,
+        ease: "linear"
+     }).totalProgress(0.5)
+
+
+
+    // gsap.to('.icon-front', {
+    //     scrollTrigger: {
+    //         trigger: ".icon-front",
+    //         scrub: true,
+    //     },
+    //     yPercent: 10,
+
+    // })
+
+
     // gsap.to('.icons-back', {
     //     scrollTrigger:{
     //         trigger: ".icons-back",
@@ -59,20 +41,21 @@ window.addEventListener('DOMContentLoaded', ()=> {
     //         scrub:true,
     //     },
     //     y: -20,
-       
+
     // })
 
- 
-    gsap.to('.image-dollar', {
-        scrollTrigger:{
-            trigger: ".image-dollar",
-            scrub:true,
-        },
-        yPercent: -10,
-        duration: .3,
-       
-    })
-  
+
+    // gsap.to('.image-dollar', {
+    //     scrollTrigger: {
+    //         trigger: ".image-dollar",
+    //         scrub: true,
+    //     },
+    //     yPercent: -10,
+    //     duration: .3,
+
+    // })
+
+
 
 
 })
